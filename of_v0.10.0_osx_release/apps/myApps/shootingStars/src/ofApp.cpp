1ca97930c22608a6df1e132f8352750e0fc42716
ashwath22 <ashwath.anand.22@gmail.com>
2018-12-02 21:48:53 -0800

    luxi

--- of_v0.10.0_osx_release/apps/myApps/shootingStars/src/ofApp.cpp
+++ of_v0.10.0_osx_release/apps/myApps/shootingStars/src/ofApp.cpp
@@ -21,13 +21,13 @@ Params param;        //Definition of global variable
 void Params::setup() {
     eCenter = ofPoint( ofGetWidth() / 2, ofGetHeight() / 2 );
     eRad = 2000;
-    velRad = 5000;
+    velRad = 1000;
     lifeTime = 10.0;
-    rotate = 120;
+    rotate = -120;
     
-    force = 100;
-    spinning = 100;
-    friction = 0.15;
+    force = -200;
+    spinning = -100;
+    friction = 0.22;
     
     eCenter2 = ofPoint( ofGetWidth() / 2, ofGetHeight() / 2 );
     eRad2 = 12000;
@@ -35,7 +35,7 @@ void Params::setup() {
     lifeTime2 = 10.0;
     rotate2 = 100;
     
-    force2 = 100;
+    force2 = 1000;
     spinning2 = 100;
     friction2 = 0.1;
     
@@ -151,7 +151,7 @@ void Particle::draw(){
         
         //Compute color
         ofColor color = ofColor::yellow;
-        float hue = ofMap( time, 0, lifeTime, 180, 250 );
+        float hue = ofMap( time, 0, lifeTime, 130, 220 );
         color.setHue( hue );
         ofSetColor( color );
         
@@ -196,8 +196,8 @@ void ofApp::setup(){
     //Set up parameters
     param.setup();        //Global parameters
     history = 10.0;
-    bornRate = 50;
-    bornCount = 10;
+    bornRate = 10;
+    bornCount = 5;
     
     time0 = ofGetElapsedTimef();
     history2 = 0.001;
@@ -278,7 +278,7 @@ void ofApp::update(){
     }
     
     //        thread.lock();
-    if (msg[0] > 0.6) {
+    if (msg[0] > 0.7) {
         if (toggle == 0){
             thread.startThread();
             thread2.startThread();
@@ -384,7 +384,7 @@ void ofApp::draw(){
     ofFill();
     //    int toggle =0;
     for (int i=0; i<p.size(); i++) {
-        if (msg[0] > 0.6) {
+        if (msg[0] > 0.7) {
             //            if (toggle == 0){
             p[i].draw2();
             //            }
